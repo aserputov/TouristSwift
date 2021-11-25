7c6efc14986a86394241ec7c32c98ca83a7ee721
Anatoliy Serputov <aserputov@myseneca.ca>
2021-11-25 07:08:39 -0500

    enhancement: Custom TableView file added.

---
+++ CustomTableView.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableView.swift
+//  Tourist
+//
+//  Created by Anatoliy Serputov on 2021-11-25.
+//
+
+import UIKit
+
+class CustomTableView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Tourist/CustomTableView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTableView.swift
+//  Tourist
+//
+//  Created by Anatoliy Serputov on 2021-11-25.
+//
+
+import Foundation
