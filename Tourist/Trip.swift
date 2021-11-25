041226edc4c7d64091ade464ffe2bea5dd7304e5
Anatoliy Serputov <aserputov@myseneca.ca>
2021-11-25 01:27:01 -0500

    enhancement: Databases connectred. Two screen App built. README.md

---
+++ Tourist/Trip.swift
@@ -0,0 +1,19 @@
+//
+//  trip.swift
+//  Tourist
+//
+//  Created by Anatoliy Serputov on 2021-11-25.
+//
+
+import Foundation
+import FirebaseFirestoreSwift
+
+
+struct Trip:Codable{
+    @DocumentID var id:String
+    var title:String = "";
+    var stars:Int = 0;
+    var price:Int = 0;
+    var images:String = ""
+    
+}
--- Tourist/ViewController.swift
+++ Tourist/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
+    
+    let db = Firestore.firestore()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func get(_ sender: Any) {
+        db.collection("activities").getDocuments { (results, error) in
+            if let err = error{
+                print("shot")
+                print(err)
+                return
+            }
+            print(results!.count)
+            
+            for document in results!.documents{
+                let dc = document.data()
+                print(dc["price"])
+                print(dc["title"])
+                print(dc["stars"])
+            }
+         
+        }
+    }
 }
 
