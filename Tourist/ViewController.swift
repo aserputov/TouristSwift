e81278f0300e18d2cab6d579d8f65da7b0a8ae85
Anatoliy Serputov <aserputov@myseneca.ca>
2021-11-25 04:55:04 -0500

    enhancement: TableViewAdded. New rows created.

--- Tourist/ViewController.swift
+++ Tourist/ViewController.swift
@@ -8,14 +8,42 @@
 import UIKit
 import FirebaseFirestore
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
+    @IBOutlet weak var MyTable: UITableView!
     let db = Firestore.firestore()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        MyTable.dataSource = self
+        MyTable.delegate = self
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {return 4 }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = MyTable.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        print(indexPath.row)
+        
+        cell.textLabel!.text = "OKSSKSKSK"
+        cell.detailTextLabel!.text = "ok"
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        print(indexPath.row)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){}
     
 
     @IBAction func get(_ sender: Any) {
